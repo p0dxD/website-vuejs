696083d1705237345bf964613fc20d9529f08b25
Jose Rodriguez <jose0797@gmail.com>
2020-03-29 14:11:49 -0400

    Testing

--- back-end/src/main/java/com/joserod/space/websitebackend/configs/KeyStoreConfiguration.java
+++ back-end/src/main/java/com/joserod/space/websitebackend/configs/KeyStoreConfiguration.java
@@ -22,7 +22,7 @@ class KeyStoreConfiguration {
 
     private static final String JKS_LOCATION_VAR = "JKS_ROOT_DIRECTORY";
 
-    private static final String FILE_TYPE = "CERTIFICATE_FILE_TYPE";
+    private static final String FILE_TYPE = "CERTIFICATE_JAVA_FILE_TYPE";
 
     static final String JAVA_KEYSTORE_PROPERTY = "javax.net.ssl.keyStore";
     static final String JAVA_KEYSTORE_PASSWORD_PROPERTY = "javax.net.ssl.keyStorePassword";
@@ -33,8 +33,8 @@ class KeyStoreConfiguration {
     private static final Logger logger = LoggerFactory.getLogger(KeyStoreConfiguration.class);
 
     static {
-        setJKSKeystore(KEYSTORE_VAR, JAVA_KEYSTORE_PROPERTY, KEYSTORE_PASSWORD_VAR, JAVA_KEYSTORE_PASSWORD_PROPERTY, FILE_TYPE);
-        setJKSKeystore(TRUSTSTORE_VAR, JAVA_TRUSTSTORE_PROPERTY, TRUSTSTORE_PASSWORD_VAR, JAVA_TRUSTSTORE_PASSWORD_PROPERTY, FILE_TYPE);
+        setJKSKeystore(KEYSTORE_VAR, JAVA_KEYSTORE_PROPERTY, KEYSTORE_PASSWORD_VAR, JAVA_KEYSTORE_PASSWORD_PROPERTY);
+        setJKSKeystore(TRUSTSTORE_VAR, JAVA_TRUSTSTORE_PROPERTY, TRUSTSTORE_PASSWORD_VAR, JAVA_TRUSTSTORE_PASSWORD_PROPERTY);
     }
 
     /**
@@ -44,13 +44,13 @@ class KeyStoreConfiguration {
      * If the environment variable named by {@param passwordEnvVar} is set, the value of this environment variable
      * is set for the system property specified by {@param systemPasswordProperty}.
      */
-    static void setJKSKeystore(String envVar, String systemProperty, String passwordEnvVar, String systemPasswordProperty, String fileType) {
+    static void setJKSKeystore(String envVar, String systemProperty, String passwordEnvVar, String systemPasswordProperty) {
         final String encodedJks = System.getenv(envVar);
-        System.out.println("KEYSTORE_VAR: " + envVar);
+        System.out.println("KEYSTORE_VAR: " + envVar + System.getenv(envVar));
         System.out.println("JAVA_KEYSTORE_PROPERTY: " + systemProperty);
-        System.out.println("KEYSTORE_PASSWORD_VAR: " + passwordEnvVar);
-        System.out.println("JAVA_KEYSTORE_PASSWORD_PROPERTY: " + systemPasswordProperty);
-        System.out.println("FILE_TYPE: " + fileType);
+        System.out.println("KEYSTORE_PASSWORD_VAR: " + passwordEnvVar + System.getenv(passwordEnvVar));
+        System.out.println("JAVA_KEYSTORE_PASSWORD_PROPERTY: " + systemPasswordProperty + System.getenv(systemPasswordProperty));
+        System.out.println("FILE_TYPE: "  + System.getenv(FILE_TYPE));
         if (StringUtils.isEmpty(encodedJks)) {
             logger.info("No " + envVar + " environment variable defined");
             return;
@@ -59,12 +59,12 @@ class KeyStoreConfiguration {
         // allow override of the destination directory
         String dir = System.getenv(JKS_LOCATION_VAR);
         if (StringUtils.isEmpty(dir)) {
-            dir = "/home/vcap/";
+            dir = "/home/";
         }
-        final String fileTypeEnding = System.getenv(fileType);
+        final String fileType = System.getenv(FILE_TYPE);
         byte[] jks = Base64Utils.decodeFromString(encodedJks);
         try {
-            String location = Paths.get(dir, envVar.toLowerCase()) + "." + fileTypeEnding;
+            String location = Paths.get(dir, envVar.toLowerCase()) + "." + fileType;
             try (OutputStream stream = new FileOutputStream(location)) {
                 stream.write(jks);
             }
